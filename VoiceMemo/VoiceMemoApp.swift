f6d17bed2fc9534f7b341d6bbd3a30ac27744525
Cjungwo <jwoo516@naver.com>
2024-03-11 21:54:07 +1100

    Initial Commit

---
+++ VoiceMemo/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by JungWoo Choi on 11/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
